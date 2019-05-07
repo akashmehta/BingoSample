2c3405936a7566d5684d65d7b117b3f3f90d7289
akashmehta <akashmehta4444@gmail.com>
2019-05-07 14:48:25 +0530

    Refactor logic to store tile coordinates.

--- app/src/main/java/com/aakash/bingosample/MainActivity.java
+++ app/src/main/java/com/aakash/bingosample/MainActivity.java
@@ -1,17 +1,13 @@
 package com.aakash.bingosample;
 
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Pair;
-import android.view.MotionEvent;
 import android.view.View;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     String[] array2 = new String[] {"A", "F", "C", "G"};
 
     ArrayList<String[]> arrays = new ArrayList<>();
-    ArrayList<Pair<Integer,Integer>> coordinates = new ArrayList<>();
+    ArrayList<Pair<Integer,Integer>> tileCoordinate = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,14 +52,11 @@ public class MainActivity extends AppCompatActivity {
                             for (int i = 0; i < arrays.size(); i++) {
                                 ArrayList<Pair<Integer, Integer>> pairs = new ArrayList<>();
                                 for (int j = 0; j < arrays.get(i).length; j++) {
-                                    Pair<Integer, Integer> coordinates = getCoordinate(layoutManager.findViewByPosition(charList.indexOf(arrays.get(i)[j])));
-                                    pairs.add(coordinates);
+                                    pairs.add(tileCoordinate.get(charList.indexOf(arrays.get(i)[j])));
                                 }
                                 paths.add(pairs);
                             }
-
                             ArrayList<ArrayList<Pair<Integer,Integer>>> viewPaths = new ArrayList();
-
                             for (int j = 0; j < arrays.size(); j++) {
                                 for (int k = 0; k < arrays.get(j).length; k++) {
                                     if (arrays.get(j)[k].contains(charList.get(finalI))) {
@@ -79,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
 
                 for (int i = 0; i < charList.size(); i++) {
                     Pair<Integer, Integer> coordinates = getCoordinate(layoutManager.findViewByPosition(i));
-                    MainActivity.this.coordinates.add(coordinates);
+                    tileCoordinate.add(coordinates);
                 }
 
 
@@ -87,8 +80,7 @@ public class MainActivity extends AppCompatActivity {
                 for (int i = 0; i < arrays.size(); i++) {
                     ArrayList<Pair<Integer, Integer>> pairs = new ArrayList<>();
                     for (int j = 0; j < arrays.get(i).length; j++) {
-                        Pair<Integer, Integer> coordinates = getCoordinate(layoutManager.findViewByPosition(charList.indexOf(arrays.get(i)[j])));
-                        pairs.add(coordinates);
+                        pairs.add(tileCoordinate.get(charList.indexOf(arrays.get(i)[j])));
                     }
                     paths.add(pairs);
                 }
